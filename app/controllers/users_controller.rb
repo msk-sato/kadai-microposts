51e97b1b6974fe2ce5702365f03d8d771b7890e0
misaki.sato <2015outoc@gmail.com>
2020-07-13 08:40:12 +0000

    favoritesタブ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
   def likes
     @user = User.find(params[:id])
     @likes = current_user.likes.page(params[:page])
+    counts(@user)
   end
   
   private
